45ec9a09e58c682ea5d0a5e53735808c3ec2db42
Aayush141 <79538073+Aayush141@users.noreply.github.com>
2023-02-23 00:49:23 +0000

    workin

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,3 +1,19 @@
 package dfa // leave this line in the file
 
-// TODO: replace this comment with your implementation
+case class State(label: String)
+
+case class Transition(from: State, to: State, symbol: Char)
+
+class DFA(val states: Set[State], val transitions: Set[Transition],
+    val start: State, val accept: Set[State]):
+    
+    def accepts(input: String): Boolean = {
+        var currentState = start
+
+        for (c <- input) {
+            val currTemp = (transitions.filter(_.from == currentState).filter(_.symbol == c)).head.to
+            currentState = currTemp
+        }
+
+        accept.contains(currentState)
+    }
