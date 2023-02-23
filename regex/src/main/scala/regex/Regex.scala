ca4bf62064cde31fcffc2a82ad5332c673d28c57
Aayush141 <79538073+Aayush141@users.noreply.github.com>
2023-02-23 07:51:18 +0000

    still got some

--- regex/src/main/scala/regex/Regex.scala
+++ regex/src/main/scala/regex/Regex.scala
@@ -8,6 +8,20 @@ package regex
 
 // Add your definitions here
 
+trait RegularLanguage
+
+//case objects
+case object Empty extends RegularLanguage
+case object Epsilon extends RegularLanguage
+
+//case classes
+//Character, Union, Concat, and Star
+
+case class Character(val c1: Char) extends RegularLanguage
+case class Union(val l1: RegularLanguage, val l2: RegularLanguage) extends RegularLanguage
+case class Concat(val l1: RegularLanguage, val l2: RegularLanguage) extends RegularLanguage
+case class Star(val s1: RegularLanguage) extends RegularLanguage
+
 /** *****************************************************************************
   * Derivatives
   *
@@ -15,13 +29,36 @@ package regex
   */
 
 /** Simplifies a regular language */
-def simplify(lang: RegularLanguage): RegularLanguage = ???
+def simplify(lang: RegularLanguage): RegularLanguage = lang match
+  case (Star(Empty)) => Empty
+  case (Star(Epsilon)) => Epsilon
+  case Union(lang, Empty) => simplify(lang)
+  case Union(Empty, lang) => simplify(lang)
+  case Union(l1, l2) => Union(simplify(l1), simplify(l2))
+  case Concat(Epsilon, lang) => simplify(lang)
+  case Concat(lang, Epsilon) => simplify(lang)
+  case Concat(Empty, lang) => simplify(Empty)
+  case Concat(lang, Empty) => simplify(Empty)
+  case Concat(l1, l2) => Concat(simplify(l1), simplify(l2))
+  case Star(lang) => Star(simplify(lang))
+
+  case _ => lang
 
 /** A language is nullable if it contains ε */
-def nullable(lang: RegularLanguage): Boolean = ???
+def nullable(lang: RegularLanguage): Boolean = lang match
+  case Empty | Character(_) => false
+  case Epsilon => true
+  case Star(_) => true
+  case Concat(l1, l2) => nullable(l1) & nullable(l2)
+  case Union(l1, l2) => nullable(l1) | nullable(l2)
+
+  case _ => nullable(simplify(lang))
+
+
 
 /** Computes the derivative of a language, with respect to a character */
 def derivative(l: RegularLanguage)(c: Char): RegularLanguage = ???
+
 
 /** *****************************************************************************
   * String-matching with regular expressions
